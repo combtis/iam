35e277a5091af64761cbaffbb8bfc4a97e874865
combtis <130187160+combtis@users.noreply.github.com>
2026-03-01 09:04:54 +0300

    add: crypto/ed25519, encoding/pem

--- iam.go
+++ iam.go
@@ -3,6 +3,13 @@
 package iam
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"net/http"
 	"os"
 )
@@ -10,6 +17,55 @@ import (
 var (
 	site = os.Getenv("IAM_SITE")
 )
+
+type (
+	OpenIDConfig struct {
+		Endpoint  string `json:"endpoint"`
+		PublicKey []byte `json:"public_key"`
+	}
+)
+
+func NewOpenIDConfig() *OpenIDConfig {
+	oidc := &OpenIDConfig{}
+
+	return oidc
+}
+
+func (oidc *OpenIDConfig) GenerateKey() (pub ed25519.PublicKey, priv ed25519.PrivateKey, err error) {
+	pub, priv, err = ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	return
+}
+
+func (oidc *OpenIDConfig) LoadPrivateKey(filename string) error {
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0o644)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(f)
+	f.Close()
+
+	block, rest := pem.Decode(buf.Bytes())
+	if block == nil {
+		return errors.New("err: file not pem format")
+	}
+	_ = rest
+
+	if block.Type != "PUBLIC KEY" {
+		return errors.New("err: file not public key")
+	}
+
+	oidc.PublicKey = []byte(base64.RawURLEncoding.EncodeToString([]byte(block.Bytes)))
+
+	return nil
+}
+
+func (oidc *OpenIDConfig) ToJSON() ([]byte, error) {
+	return json.Marshal(oidc)
+}
 
 func OpenIDConfiguration(rw http.ResponseWriter, req *http.Request) {
 	code := http.StatusOK
