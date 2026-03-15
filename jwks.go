ece9dc28d6180fff15fc0282f6a47713aac7ce7c
combtis <130187160+combtis@users.noreply.github.com>
2026-03-15 23:00:25 +0300

    add jwks

--- jwks.go
+++ jwks.go
@@ -4,12 +4,14 @@ package iam
 
 import (
 	"bytes"
-	"crypto/ed25519"
+	ed "crypto/ed25519"
 	"crypto/rand"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"math/big"
 	"net/http"
 	"os"
 )
@@ -20,11 +22,83 @@ type (
 	}
 
 	Key struct {
-		Alg string `json:"alg"`
-		E   string `json:"e"`
-		Kid string `json:"kid"`
-		Kty string `json:"kty"`
-		N   string `json:"n"`
-		Use string `json:"use"`
+		Kty string `json:"kty,omitempty"` // type
+		Use string `json:"use,omitempty"` // use?
+		Alg string `json:"alg,omitempty"` // algoritmh
+		Kid string `json:"kid,omitempty"` // key id
+		Crv string `json:"crv,omitempty"` // curve
+		X   string `json:"x,omitempty"`   // .
 	}
 )
+
+func NewJWKS() *JWKS {
+	jwks := &JWKS{
+		Keys: []Key{},
+	}
+	_ = http.DefaultClient
+	_ = big.Int{}
+
+	return jwks
+}
+
+func (jwks *JWKS) GenerateKey() (ed.PublicKey, ed.PrivateKey, error) {
+	return ed.GenerateKey(rand.Reader)
+}
+
+func (jwks *JWKS) Load(filename string) error {
+	if _, err := os.Stat(filename); err != nil {
+		return err
+	}
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0o600)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(f)
+	f.Close()
+
+	block, _ := pem.Decode(buf.Bytes())
+	if block == nil {
+		return errors.New("decode not pem format")
+	}
+
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+	pub := (priv).(ed.PrivateKey).Public().(ed.PublicKey)
+
+	jwks.Keys = append(jwks.Keys, Key{
+		Kty: "OKP",
+		Use: "sig",
+		Alg: "EdDSA",
+		Kid: filename,
+		Crv: "OKP",
+		X:   base64.RawURLEncoding.EncodeToString([]byte(pub)),
+	})
+
+	return nil
+}
+
+func (jwks *JWKS) String() string {
+	bs, err := json.MarshalIndent(jwks, "", "  ")
+	if err != nil {
+		return ""
+	}
+	return string(bs)
+}
+
+func (jwks *JWKS) InsertPrivateKey(priv ed.PrivateKey) {
+	public, ok := priv.Public().(ed.PublicKey)
+	if !ok {
+		return
+	}
+	key := Key{
+		Kty: "OKP",
+		Use: "sig",
+		Alg: "EdDSA",
+		Crv: "OKP",
+		X:   base64.RawURLEncoding.EncodeToString([]byte(public)),
+	}
+	jwks.Keys = append(jwks.Keys, key)
+}
---
+++ jwks_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2026 Business Technologies
+// SDPX-Licencse-Identifier: MIT
+package iam
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	t.Log("iam testing...\n")
+
+	jwks := NewJWKS()
+	pub, priv, err := jwks.GenerateKey()
+	if err == nil {
+		//jwks.Load("testdata/private")
+		_ = pub
+		jwks.InsertPrivateKey(priv)
+		t.Log(jwks.String())
+	}
+}
